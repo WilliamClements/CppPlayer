a998756998188cd85dfa941973bd8342a618b97c
Andrew Bushnell <andrewbushnellnh@gmail.com>
2023-01-26 15:28:18 -0500

    Update for CPP 17 (#30)

--- include/NamespaceAliases.hpp
+++ include/NamespaceAliases.hpp
@@ -20,6 +20,16 @@ limitations under the License.
 
 #pragma once
 
-#include <experimental/filesystem>
+// For C++ 17 and above use built in filesystem
+#if ((defined(_MSVC_LANG) && _MSVC_LANG >= 201703L) || __cplusplus >= 201703L)
 
+#include <filesystem>
+namespace fs = std::filesystem;
+
+#else
+
+#include <experimental/filesystem>
 namespace fs = std::experimental::filesystem::v1;
+
+#endif
+
